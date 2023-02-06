a7c8d65d67045b5f87018b54ecaa02e50358d5c7
Dave Phillips <davephilpz@yahoo.com>
2023-02-05 20:49:07 -0500

    before implementing pagination

---
+++ public/js/reviewCards.js
@@ -0,0 +1,12 @@
+document.getElementById("previous-word").addEventListener("click", function () {
+  //   if (currentPage > 0) {
+  //     currentPage--;
+  console.log("prev page");
+  //   }
+});
+document.getElementById("next-word").addEventListener("click", function () {
+  //   if (currentPage < searchResults.length - 1) {
+  //     currentPage++;
+  console.log("next page");
+  //   }
+});
