beb1f0e13fc28376b8c0ac114ca5cd76d39563b9
Dave Phillips <davephilpz@yahoo.com>
2023-02-16 22:12:16 -0500

    added rej/exc safety nets with logging and refactored/commented server.js

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+//basic delcarations
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-const port = process.env.PORT || 3000;
 const app = require("./app");
-dotenv.config({ path: "./config.env" });
+require("dotenv").config({ path: "./config.env" });
 
+//logger
+const winston = require("winston");
+
+//server port
+const port = process.env.PORT || 3000;
+
+//initialiaze logger to track errors
+const unhandledRejectionLogger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/unhandled-rejections.log" }),
+  ],
+});
+
+const unhandledExceptionLogger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/unhandled-exceptions.log" }),
+  ],
+});
+
+//database config with mongoose framework
 mongoose.set("strictQuery", false);
 const DB = process.env.LOCAL_DATABASE.replace(
   "<DATABASE_PASSWORD>",
@@ -16,14 +36,30 @@ mongoose
   })
   .then(() => console.log("DB connection successful"));
 
+//run server
 const server = app.listen(port, () => {
   console.log(`Server now running on ${port}`);
 });
 
-//catch all error safety net
+//catch all unhandled promise rejections
 process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
-  console.log("Unhandled error. Shutting down server...");
+  console.log("Unhandled rejection. Shutting down server...");
+
+  unhandledRejectionLogger.error("Unhandled Rejection", err);
+
+  //call server.close() to allow server to finish current executions before shutting down.
+  server.close(() => {
+    process.exit(1); //0 = success, 1=exception
+  });
+});
+
+//catch all unhandled errors
+process.on("unhandledException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled exception. Shutting down server...");
+
+  unhandledExceptionLogger.error("Unhandled Exception", err);
 
   //call server.close() to allow server to finish current executions before shutting down.
   server.close(() => {
